611dab5c58dc3d4ece1a5ce58ffa3732b36f6abb
Nelson Loyola <nloyola@gmail.com>
2021-03-24 10:13:36 +0100

    Applied focusonclick patch

--- config.def.h
+++ config.def.h
@@ -13,6 +13,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Fira Code Retina:size=10", "monospace:size=10" };
 static const char dmenufont[]       = "Go Mono:size=10";
 static const char col_gray1[]       = "#222222";
@@ -32,16 +33,16 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
-         *      WM_CLASS(STRING) = instance, class
-         *      WM_NAME(STRING) = title
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
 	 */
         /* ORDER MATTERS */
-        /* class            instance    title       tags mask     isfloating   monitor */
+	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Google-chrome",  NULL,       NULL,       1,            0,            0 },
 	{ "Alacritty",      NULL,       "bpytop",   1 << 1,       0,            0 },
 	{ "Alacritty",      NULL,       "ncspot",   1 << 1,       0,           -1 },
         { "Alacritty",      NULL,       NULL,       0,            0,           -1 },
-	{ "Firefox",        NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 };
 
@@ -87,20 +88,20 @@ static const char *roficmd[]       = { "rofi", "-show", "drun", "-show-icons", N
 
 #include "movestack.c"
 static Key keys[] = {
-        /* modifier                     key                       function        argument */
-        { MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,                     spawn,          {.v = roficmd } },
-        { MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
         { MODKEY,                       XK_grave,                 togglescratch,  {.v = scratchpadcmd } },
-        { MODKEY,                       XK_b,                     togglebar,      {0} },
-        { MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
-        { MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
-        { MODKEY,                       XK_i,                     incnmaster,     {.i = +1 } },
-        { MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_j,                     movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,                     movestack,      {.i = -1 } },
-        { MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
-        { MODKEY,                       XK_l,                     setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
 	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
 	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
@@ -117,31 +118,31 @@ static Key keys[] = {
 	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-        { MODKEY,                       XK_Return,                zoom,           {0} },
-        { MODKEY,                       XK_Tab,                   view,           {0} },
-        { MODKEY|ShiftMask,             XK_c,                     killclient,     {0} },
-        { MODKEY,                       XK_t,                     setlayout,      {.v = &layouts[0]} },
-        { MODKEY,                       XK_f,                     setlayout,      {.v = &layouts[1]} },
-        { MODKEY,                       XK_m,                     setlayout,      {.v = &layouts[2]} },
-        { MODKEY,                       XK_space,                 setlayout,      {0} },
-        { MODKEY|ShiftMask,             XK_space,                 togglefloating, {0} },
-        { MODKEY,                       XK_0,                     view,           {.ui = ~0 } },
-        { MODKEY|ShiftMask,             XK_0,                     tag,            {.ui = ~0 } },
-        { MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
-        { MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
-        { MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
-        { MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
-        TAGKEYS(                        XK_1,                     0)
-        TAGKEYS(                        XK_2,                     1)
-        TAGKEYS(                        XK_3,                     2)
-        TAGKEYS(                        XK_4,                     3)
-        TAGKEYS(                        XK_5,                     4)
-        TAGKEYS(                        XK_6,                     5)
-        TAGKEYS(                        XK_7,                     6)
-        TAGKEYS(                        XK_8,                     7)
-        TAGKEYS(                        XK_9,                     8)
+	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
     { MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
-        { MODKEY|ShiftMask,             XK_q,                     quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
         { 0,                            XF86XK_AudioRaiseVolume,  spawn,          {.v = volup } },
         { 0,                            XF86XK_AudioLowerVolume,  spawn,          {.v = voldown } },
         { 0,                            XF86XK_AudioPlay,         spawn,          SHCMD("playerctl play-pause") },
